04bcd7e09a3dfe7318ca06d1f1f401ec9ff678ad
Rajeesh C V <cvrajeesh@gmail.com>
2023-08-29 17:46:14 +0530

    fix(aws/s3-bucket): Include bucket domain name in the output (#2)

--- aws/modules/s3-bucket/outputs.tf
+++ aws/modules/s3-bucket/outputs.tf
@@ -7,3 +7,8 @@ output "id" {
   description = "S3 bucket name"
   value       = aws_s3_bucket.this.id
 }
+
+output "domain_name" {
+  description = "S3 bucket domain name"
+  value       = aws_s3_bucket.this.bucket_domain_name
+}
